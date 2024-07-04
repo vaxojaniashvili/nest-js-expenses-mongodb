362828e289a1a2970d632f703c484d6754119f51
vaxo <vaxojaniashvili186@gmail.com>
2024-07-04 23:39:27 +0400

    starting-nest-js-mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ExpensesModule } from './expenses/expenses.module';
 
 @Module({
-  imports: [ExpensesModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
